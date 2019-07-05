7c23bf345b290f5ac2647f7f7d6d3f23a5565f3b
alan-camilo <alan.c.herrera@gmail.com>
2019-07-05 04:45:06 +0200

    take and save picture locally

--- app/src/main/java/fr/camilo/rockstarsapp/activity/ProfileActivity.kt
+++ app/src/main/java/fr/camilo/rockstarsapp/activity/ProfileActivity.kt
@@ -1,15 +1,39 @@
 package fr.camilo.rockstarsapp.activity
 
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
+import androidx.exifinterface.media.ExifInterface
 import fr.camilo.rockstarsapp.R
+import kotlinx.android.synthetic.main.activity_profile.*
 import kotlinx.android.synthetic.main.navigator.*
-
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ProfileActivity : AppCompatActivity() {
+
+    val PERMISSION_REQUEST_CODE = 2
+    val REQUEST_IMAGE_CAPTURE = 1
+    var currentPhotoPath: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +48,86 @@ class ProfileActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+        profile_picture.setOnClickListener {
+            if (checkPersmission()) takePicture() else requestPermission()
+        }
+    }
+
+    private fun takePicture() {
+
+        val intent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        val file: File = createFile()
+
+        val uri: Uri = FileProvider.getUriForFile(
+            this,
+            "fr.camilo.rockstarsapp.fileprovider",
+            file
+        )
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri)
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+
+    }
+
+    fun rotateImage(source: Bitmap, angle: Float): Bitmap {
+        val matrix = Matrix()
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(
+            source, 0, 0, source.getWidth(), source.getHeight(),
+            matrix, true
+        );
+    }
+
+    @Throws(IOException::class)
+    private fun createFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+
+            //To get the File for further usage
+            val auxFile = File(currentPhotoPath)
+
+            val bitmap: Bitmap? = BitmapFactory.decodeFile(currentPhotoPath)
+
+            Toast.makeText(this, "$currentPhotoPath", Toast.LENGTH_LONG).show()
+            Log.d("PROFILE_ACTIVITY", "$currentPhotoPath")
+
+
+            val ei = ExifInterface(currentPhotoPath)
+            val orientation = ei.getAttributeInt(
+                ExifInterface.TAG_ORIENTATION,
+                ExifInterface.ORIENTATION_UNDEFINED
+            )
+
+            var rotatedBitmap: Bitmap? = null
+            when (orientation) {
+                ExifInterface.ORIENTATION_ROTATE_90 ->
+                    rotatedBitmap = rotateImage(bitmap!!, 90F)
+
+                ExifInterface.ORIENTATION_ROTATE_180 ->
+                    rotatedBitmap = rotateImage(bitmap!!, 180F);
+
+                ExifInterface.ORIENTATION_ROTATE_270 ->
+                    rotatedBitmap = rotateImage(bitmap!!, 270F);
+
+                else -> rotatedBitmap = bitmap
+            }
+            if (rotatedBitmap != null) {
+                profile_picture.setImageBitmap(rotatedBitmap)
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -36,5 +140,42 @@ class ProfileActivity : AppCompatActivity() {
             R.id.action_save -> Unit //TODO
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun checkPersmission(): Boolean {
+        return (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf(READ_EXTERNAL_STORAGE, CAMERA),
+            PERMISSION_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED
+                ) {
+
+                    takePicture()
+
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+
+            else -> {
+
+            }
+        }
     }
 }
